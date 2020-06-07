36849c69d7fbca8796de61329421516a6a0dffae
Bertram Gilfoy <nonils@tutanota.com>
2020-06-07 00:29:01 -0300

    Add configuration to localserver with config serever .

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var appName = "infomationserver"
 
 func init() {
 	profile := flag.String("profile", "dev", "Environment profile, something similar to spring profiles")
-	configServerUrl := flag.String("configServerUrl", "https://med-config-server.herokuapp.com", "Address to config server")
+	configServerUrl := flag.String("configServerUrl", "http://localhost:8888", "Address to config server")
 	configBranch := flag.String("configBranch", "master", "git branch to fetch configuration from")
 	flag.Parse()
 	fmt.Println("Specified configBranch is " + *configBranch)
